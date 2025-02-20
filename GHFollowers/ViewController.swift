a40f23f9cb574305b4db5f17359dfb89e38a2973
Rodrigo Porto <rodrigo.porto@watchguard.com>
2025-02-20 11:31:08 -0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Porto on 20/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
