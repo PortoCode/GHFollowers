b389dffeda873794848cb229099ef182f8bef398
Rodrigo Porto <rodrigo.porto@watchguard.com>
2025-02-20 11:54:12 -0300

    refactor: remove main storyboard

--- GHFollowers/ViewController.swift
+++ GHFollowers/ViewController.swift
@@ -11,7 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGray5
+        
+        let label = UILabel()
+        label.text = "GHFollowers"
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        UIView.animate(withDuration: 1.5, delay: 0, options: [.autoreverse, .repeat], animations: {
+            label.alpha = 0
+        }, completion: nil)
     }
 
 
