09a4eb63ecddea4862fc843d0ab8f3f8c4ebdda3
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-03 21:13:14 -0300

    feat: add follower and user models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Porto on 03/04/25.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Porto on 03/04/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatar_url: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var followers: Int
+    var following: Int
+    var createdAt: String
+}
