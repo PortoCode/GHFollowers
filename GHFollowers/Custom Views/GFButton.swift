abed20928f9a85ad1c88599da67a6bdfa16d14a5
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-04 00:48:54 -0300

    refactor: update call set title color

--- GHFollowers/Custom Views/GFButton.swift
+++ GHFollowers/Custom Views/GFButton.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class GFButton: UIButton {
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -27,9 +27,9 @@ class GFButton: UIButton {
     
     private func configure() {
         layer.cornerRadius = 10
-        titleLabel?.textColor = .white
+        setTitleColor(.white, for: .normal)
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
     }
-
+    
 }
---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Porto on 04/04/25.
+//
+
+import Foundation
