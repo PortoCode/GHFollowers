de64b6e149c35cac1e4b21480caa0500afd87eb6
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-06 16:55:15 -0300

    feat: caching avatar images

--- GHFollowers/Custom Views/GFAvatarImageView.swift
+++ GHFollowers/Custom Views/GFAvatarImageView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     
+    let cache = NetworkManager.shared.cache
     let placeholderImage = UIImage(named: "avatar-placeholder")
     
     override init(frame: CGRect) {
@@ -28,18 +29,24 @@ class GFAvatarImageView: UIImageView {
     }
     
     func downloadImage(from urlString: String) {
+        let cacheKey = urlString
+        
+        if let image = cache.object(forKey: cacheKey as NSString) {
+            self.image = image
+            return
+        }
+        
         guard let url = URL(string: urlString) else { return }
         
         let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
             guard let self = self else { return }
             
             if error != nil { return }
-            
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
-            
             guard let data = data else { return }
             
             guard let image = UIImage(data: data) else { return }
+            cache.setObject(image, forKey: cacheKey as NSString)
             
             DispatchQueue.main.async {
                 self.image = image
