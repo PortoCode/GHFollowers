392f32bde6498b112affd10377143c1286156170
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-06 23:05:12 -0300

    feat: add a reusable empty state view

--- GHFollowers/Screens/FollowersListViewController.swift
+++ GHFollowers/Screens/FollowersListViewController.swift
@@ -57,6 +57,15 @@ class FollowersListViewController: UIViewController {
             case let .success(followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them 😀."
+                    DispatchQueue.main.async {
+                        self.showEmptyState(with: message, in: self.view)
+                    }
+                    return
+                }
+                
                 self.updateData()
                 
             case let .failure(error):
