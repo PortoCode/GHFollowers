4dfb85d856f9b1eb2a09721732c5c23fc00e0029
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-12 10:03:38 -0300

    feat: add GFUserInfoHeaderViewController

--- GHFollowers/Screens/UserInfoViewController.swift
+++ GHFollowers/Screens/UserInfoViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
     
+    let headerView = UIView()
+    
     var username: String!
     
     override func viewDidLoad() {
@@ -17,17 +19,40 @@ class UserInfoViewController: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
         navigationItem.rightBarButtonItem = doneButton
         
+        layoutUI()
+        
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case let .success(user):
-                print(user)
+                DispatchQueue.main.async {
+                    self.add(childViewController: GFUserInfoHeaderViewController(user: user), to: self.headerView)
+                }
                 
             case let .failure(error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func add(childViewController: UIViewController, to containerView: UIView) {
+        addChild(childViewController)
+        containerView.addSubview(childViewController.view)
+        childViewController.view.frame = containerView.bounds
+        childViewController.didMove(toParent: self)
     }
     
     @objc func dismissViewController() {
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Rodrigo Porto on 12/04/25.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
