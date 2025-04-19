a789ad5f3f150ec53118c6e468473fc1177fa3fa
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-18 22:47:38 -0300

    refactor: add GFDataLoadingViewController

--- GHFollowers/Screens/FollowersListViewController.swift
+++ GHFollowers/Screens/FollowersListViewController.swift
@@ -11,7 +11,7 @@ protocol FollowersListViewControllerDelegate: AnyObject {
     func didRequestFollowers(for username: String)
 }
 
-class FollowersListViewController: UIViewController {
+class FollowersListViewController: GFDataLoadingViewController {
     
     enum Section {
         case main
