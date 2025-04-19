7ecc2a83f7597c143dcb94eadd753e58ac2eaf79
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-19 16:37:55 -0300

    fix: pagination bug fix

--- GHFollowers/Screens/FollowersListViewController.swift
+++ GHFollowers/Screens/FollowersListViewController.swift
@@ -23,6 +23,7 @@ class FollowersListViewController: GFDataLoadingViewController {
     var page = 1
     var hasMoreFollowers = true
     var isSearching = false
+    var isLoadingMoreFollowers = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -83,6 +84,8 @@ class FollowersListViewController: GFDataLoadingViewController {
     
     func getFollowers(username: String, page: Int) {
         showLoadingView()
+        isLoadingMoreFollowers = true
+        
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
             self.dismissLoadingView()
@@ -105,6 +108,8 @@ class FollowersListViewController: GFDataLoadingViewController {
             case let .failure(error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
+            
+            self.isLoadingMoreFollowers = false
         }
     }
     
@@ -163,7 +168,7 @@ extension FollowersListViewController: UICollectionViewDelegate {
         let height = scrollView.frame.size.height
         
         if offsetY > contentHeight - height {
-            guard hasMoreFollowers else { return }
+            guard hasMoreFollowers, !isLoadingMoreFollowers else { return }
             page += 1
             getFollowers(username: username, page: page)
         }
@@ -207,6 +212,7 @@ extension FollowersListViewController: FollowersListViewControllerDelegate {
         page = 1
         hasMoreFollowers = true
         isSearching = false
+        isLoadingMoreFollowers = false
         followers.removeAll()
         filteredFollowers.removeAll()
         updateData(on: followers)
