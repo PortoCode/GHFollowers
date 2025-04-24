bed68381397c112583bcfc59261dc6c1f6ea6ea6
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2025-04-23 21:35:07 -0300

    feat: create CachedAsyncImage to avoid repeated image requests

--- GHFollowers/Custom Views/Cells/FollowerCell/FollowerView.swift
+++ GHFollowers/Custom Views/Cells/FollowerCell/FollowerView.swift
@@ -7,23 +7,48 @@
 
 import SwiftUI
 
+struct CachedAsyncImage: View {
+    let url: String
+    let placeholder: Image
+    
+    @State private var uiImage: UIImage? = nil
+    @State private var hasStartedLoading = false
+    
+    var body: some View {
+        Group {
+            if let uiImage = uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+            } else {
+                placeholder
+                    .resizable()
+                    .onAppear {
+                        if !hasStartedLoading {
+                            hasStartedLoading = true
+                            loadImage()
+                        }
+                    }
+            }
+        }
+    }
+    
+    private func loadImage() {
+        Task {
+            self.uiImage = await ImageCacheManager.shared.downloadImage(from: url)
+        }
+    }
+}
+
 struct FollowerView: View {
     
     var follower: Follower
     
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string: follower.avatarUrl)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            } placeholder: {
-                Image(.avatarPlaceholder)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            }
-            .clipShape(Rectangle())
-            .cornerRadius(12)
+            CachedAsyncImage(url: follower.avatarUrl, placeholder: Image(.avatarPlaceholder))
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Rectangle())
+                .cornerRadius(12)
             
             Text(follower.login)
                 .bold()
